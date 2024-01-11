984da48820a8afae2e4da2b436be338861003a8b
xyugen <alex.arias.jorym@gmail.com>
2024-01-11 18:10:01 +0800

    add verification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { EmbedBuilder, Client, Events, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: [Partials.Channel, Partials.Message, Partials.Reaction],
+});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -28,6 +37,78 @@ client.on("interactionCreate", async (interaction) => {
         await interaction.reply(`The decision is: ${decision}`);
     }
 });
+
+/* client.on("messageCreate", async (message) => {
+    const embed = new EmbedBuilder()
+      .setTitle("Verification")
+      .setColor(0x6cba48)
+      .setDescription("React with ✅ to verify yourself.");
+    if (message.content.toLowerCase() === "!verify") {
+        const verificationMessage = await message.channel.send({ embeds: [embed] });
+        verificationMessage.react("✅");
+    }
+}); */
+
+const verificationMessageId = "1194945458137731153";
+
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+    if (
+        reaction.message &&
+        reaction.message.id === verificationMessageId &&
+        reaction.emoji.name !== "✅"
+    ) {
+        reaction.remove();
+        return;
+    }
+    
+    if (
+        reaction.message &&
+        reaction.message.id === verificationMessageId &&
+        reaction.emoji.name === '✅'
+    ) {
+        console.log("Verifying user...");
+        const message = await user.send("You are being verified...");
+
+        // Get the user who reacted
+        const verifiedUser = reaction.message.guild.members.cache.get(user.id);
+
+        // Assign the "verified" role
+        const verifiedRole = reaction.message.guild.roles.cache.find((role) => role.name === "verified");
+        
+        try {
+            if (verifiedRole) {
+                verifiedUser.roles.add(verifiedRole);
+                console.log((`User ${user.tag} has been verified.`));
+                message.edit("You have been verified in IT Dark Room!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
+
+client.on(Events.MessageReactionRemove, async (reaction, user) => {
+    if (
+        reaction.message &&
+        reaction.message.id === verificationMessageId &&
+        reaction.emoji.name === '✅'
+    ) {
+        // Get the user who reacted
+        const verifiedUser = reaction.message.guild.members.cache.get(user.id);
+
+        // Remove the "verified" role
+        const verifiedRole = reaction.message.guild.roles.cache.find((role) => role.name === "verified");
+
+        try {
+            if (verifiedRole && verifiedUser.roles.cache.has(verifiedRole.id)) {
+                verifiedUser.roles.remove(verifiedRole);
+                console.log(`User ${user.tag} has been unverified.`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+})
 
 const randomNumberInRange = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
